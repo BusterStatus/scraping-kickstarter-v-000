9d4dc42b8edc02d54d6f426154086019ac50e505
matthew.serre <matthew.serre@gmail.com>
2018-11-26 02:31:00 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,6 +1,7 @@
 require 'nokogiri'
 
 # projects: kickstarter.css("li.project.grid_4")
+# title: project.css("h2.bbcard_name strong a").texts
 
 def create_project_hash
   html = File.read("fixtures/kickstarter.html")
